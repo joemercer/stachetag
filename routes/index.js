7459c467901c6d66f978c96c7bacd65c6ee0cc78
bonnieli <bonnielokli@gmail.com>
2014-01-18 15:12:57 -0500

    integrate mongo - need to create directory data in root, do not commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
 var app = express();
 
+// DB STUFF
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/stachetag');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//GET
 app.get('/', function(req, res){
   res.send('Hello World');
 });
@@ -8,6 +29,7 @@ app.get('/', function(req, res){
 app.get('/joe', function(req, res){
   res.send('Hello Joe');
 });
+app.get('/userlist', routes.userlist(db));
 
 app.listen(3000);
 console.log('Listening on port 3000');
