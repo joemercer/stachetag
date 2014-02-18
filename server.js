018a40c7ad922e0453ce7ca2a2b28162083c173e
Joe Mercer <mynameisjoemercer@gmail.com>
2014-02-18 13:27:29 -0500

    in progress

--- server.js
+++ server.js
@@ -1,128 +1,154 @@
 var express = require('express');
-var logfmt = require("logfmt");
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-var OAuth= require('oauth').OAuth;
 
 var app = express();
-// Enabling Sessions
-app.use(express.cookieParser());
-app.use(express.session({secret: "This is a secret"}));
-
-var oa = new OAuth(
-	"https://api.twitter.com/oauth/request_token",
-	"https://api.twitter.com/oauth/access_token",
-	"cCw9Tm99H3rEwfpxODQ",
-	"OHx2mlYa7LmRUFF3fTEeFCPDwyet74qy62MtlV6zU",
-	"1.0",
-	"http://127.0.0.1:3000/auth/twitter/callback",
-	"HMAC-SHA1"
-);
 
 // # DB stuff
 // __________
 
-var mongo = require('mongodb');
-var monk = require('monk');
+var mongo = require('mongoskin');
 
 // format: mongodb://user:pass@host:port/dbnam
-var mongoUri = process.env.MONGOHQ_URL || 'localhost:27017/stachetag';
+var mongoUri = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/stachetag';
 
-var db = monk(mongoUri);
+var db = mongo.db(mongoUri, {native_parser:true});
 
-// all environments
-app.set('port', process.env.PORT || 5000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var users = db.collection('users');
+var tweets = db.collection('tweets');
 
-app.use(logfmt.requestLogger());
+var me = users.insert({
+	screen_name: 'jomrcr'
+});
+
+// twitter
+// _______
+
+var twit = require("twit");
+
+// I think that access token stuff is supposed to be specific to user
+var T = new twit({
+	consumer_key: 				'75fPUk1t4ftDZ6byJRsJ0A',
+	consumer_secret: 			'sDGqUXEAiPkGoxlP9NpqyzpnsQ0fFpLxFrUdgbY2bU',
+	access_token: 				'54261089-q9gdWGvhcHAnW348z59xjo75ynyGXXjpgP46HsBYe',
+	access_token_secret: 	'38GVTtgNS3uO9Kj8cqrJiRzXOet2AUHeDtrtci1WjeMaD'
+});
+
+var stream = T.stream('user');
+
+var streamOwnersScreenName = 'jomrcr';
+
+stream.on('tweet', function (tweet) {
+
+	// find sender
+	users.findOne({
+		screen_name: tweet.user.screen_name
+	}, function(err, user){
+		if (err) return 'Error: ' + err;
+
+		// if not sender's stream then return
+		if (user.screen_name !== streamOwnersScreenName) return;
+
+		var start = tweet.text.indexOf('{');
+		var end = tweet.text.indexOf('}');
+		var thing = tweet.text.substr(start+1, start-end-1);
+
+		var logs = [];
+
+
+
+
+		// extract things
+		// things into things
+
+		// insert event(s) into events
+
+		// delete tweet if necessary
+
+		// set deleted timeout if necessary
+
+		// insert tweet into tweets
+		tweets.insert({
+			userId: user._id,
+			tweet: tweet
+		}, {safe: true}, function(err){
+			if (err) return 'Error: ' + err;
+		});
+
+	});
+
+});
+
+// routes
+// ______
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+	res.send('Homepage and stuff');
+
+	// tweets.findOne({}).on('success', function(tweet){
+	// 	res.send(tweet);
+	// }).on('error', function(err){
+	// 	res.send('Nothing in the database yet');
+	// });
+
 });
 
-app.get('/bonnie', function(req, res) {
-  res.send('Hello Bonnie!');
-});
+app.get('/users', function(req, res) {
 
-// sample app for using mongo
-
-app.get('/userlist', routes.userlist(db));
-
-//writing to the db
-app.get('/newuser', routes.newuser);
-app.post('/adduser', routes.adduser(db));
-
-//twitter
-app.get('/auth/twitter', function(req, res){
-	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-		if (error) {
-			console.log(error);
-			res.send("yeah no. didn't work.")
-		}
-		else {
-			req.session.oauth = {};
-			req.session.oauth.token = oauth_token;
-			console.log('oauth.token: ' + req.session.oauth.token);
-			req.session.oauth.token_secret = oauth_token_secret;
-			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
-			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
-		}
+	users.find({}).toArray(function(err, allUsers){
+		if (err) return 'Error: ' + err;
+		console.log(allUsers[0]);
+		res.send(allUsers);
 	});
+
 });
-app.get('/auth/twitter/callback', function(req, res, next){
-	if (req.session.oauth) {
-		req.session.oauth.verifier = req.query.oauth_verifier;
-		var oauth = req.session.oauth;
 
-		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
-		function(error, oauth_access_token, oauth_access_token_secret, results){
-			if (error){
-				console.log(error);
-				res.send("yeah something broke.");
-			} else {
-				req.session.oauth.access_token = oauth_access_token;
-				req.session.oauth,access_token_secret = oauth_access_token_secret;
-				console.log(results);
-				console.log(oauth_access_token);
-				console.log(oauth_access_token_secret);
+app.get('/users/:name', function(req, res) {
+	// return data for a specific user
 
-				req.session.oauth.user_id = results.user_id;
-				var collection = db.get('users');
-				collection.find({user_id: results.user_id}, function(err, users) {
-				  if( err || !users || users.length === 0) {
-				  	collection.insert({
-							"user_id": results.user_id,
-							"access_token" : oauth_access_token,
-							"access_token_secret" : oauth_access_token_secret
-						}, function (err, dot) {
-							if (err) {
-								res.send("There was a problem adding the information to the database.");
-							} else {
-								console.log("tada, it stored the new user");
-							}
-						});
-				  } else {
-				    console.log("User already exists");
-				  }
-				});
-			}
+	users.findOne({
+		name: 'testjomrcr2'
+	}, function(err, user){
+		if (err) return 'Error: ' + err;
+		tweets.find({userId: user._id}).toArray(function(err, allTweets){
+			if (err) return 'Error: ' + err;
+			res.send(allTweets);
+		});
+	});
 
-			res.redirect('/twitter');
-		}
-		);
-	} else
-		next(new Error("you're not supposed to be here."))
 });
-app.get('/twitter', routes.tweet(db, "17828953"));
 
+app.get('/users/:name/tweets', function(req, res) {
+	// return tweets for a specific user
+
+	users.findOne({
+		name: req.params.name
+	}, function(err, user){
+		if (err) return 'Error: ' + err;
+		tweets.find({userId: user._id}, { tweet: 1}).toArray(function(err, allTweets){
+			if (err) return 'Error: ' + err;
+			res.send(allTweets);
+		});
+	});
+
+});
+
+app.get('/users/:name/tags/:tag', function(req, res) {
+	// returns compiled logs with a given tag for a given user
+
+	users.findOne({
+		name: req.params.name
+	}, function(err, user){
+		if (err) return 'Error: ' + err;
+		tweets.find({userId: user._id}, { tweet: 1}).toArray(function(err, allTweets){
+			if (err) return 'Error: ' + err;
+			res.send(allTweets);
+		});
+	});
+
+});
+
+// start the server
+// ________________
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
